822c53ee2dc64f395e9bac0e8a9ebb53cd4e97e7
onder yerim <onder.yerim@testinium.com>
2023-10-03 13:50:07 +0300

    proxy removed

--- src/test/java/altYapi/seleniumGauge/driver/TestiniumBrowserExec.java
+++ src/test/java/altYapi/seleniumGauge/driver/TestiniumBrowserExec.java
@@ -39,13 +39,7 @@ public class TestiniumBrowserExec {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("key", key);
 
-        String PROXY = "ec2-54-154-66-64.eu-west-1.compute.amazonaws.com:3128";
-        Proxy proxy = new Proxy();
-        proxy.setProxyType(Proxy.ProxyType.MANUAL);
-        proxy.setHttpProxy(PROXY);
-        proxy.setFtpProxy(PROXY);
-        proxy.setSslProxy(PROXY);
-        capabilities.setCapability(CapabilityType.PROXY, proxy);
+
 
         //logger.info(System.getenv().toString());
         /**Properties properties = System.getProperties();
